0e51ffcca4247ed9aec65430cc2c9552a9d869b5
Gustavo Palmeira <gustavo_palmeira07@hotmail.com>
2021-09-21 22:55:28 -0300

    Store DELETE + Store UPDATE

--- api/src/main/kotlin/com/api/api/store/ServiceStore.kt
+++ api/src/main/kotlin/com/api/api/store/ServiceStore.kt
@@ -6,24 +6,64 @@ import javax.ws.rs.core.MediaType
 @Path("/store")
 class ServiceStore {
 
-    @Path("/create")
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    fun insert(store: Store?) {
-        store?.let { DAOStore.insert(it) }
-    }
-
     @Path("/list")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    fun list(): List<Store> {
-        return DAOStore.list()
+    fun listAll(): List<Store> {
+        return try {
+            DAOStore.listAll()
+        } catch (error: Exception) {
+            return emptyList()
+        }
     }
 
-    @Path("/show/{id}")
+    @Path("/list/{id}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    fun show(@PathParam("id") queryId: Int): Store {
-        return DAOStore.show(queryId)
+    fun list(@PathParam("id") queryId: Int): Store {
+        return try {
+            DAOStore.list(queryId)
+        } catch (error: Exception) {
+            return Store()
+        }
+    }
+
+    @Path("/create")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    fun insert(store: Store?): String {
+        return try {
+            store?.let { DAOStore.insert(it) }
+            "Registro inserido com sucesso."
+        } catch (error: Exception) {
+            "Não foi possível apagar a loja. Tente novamente.\n${error.message}"
+        }
+    }
+
+    @Path("/delete/{id}")
+    @DELETE
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    fun delete(@PathParam("id") queryId: Int): String {
+        return try {
+            DAOStore.delete(queryId)
+            "Registro apagado com sucesso."
+        } catch (error: Exception) {
+            "Não foi possível apagar a loja. Tente novamente.\n${error.message}"
+        }
+    }
+
+    @Path("/update/{id}")
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    fun update(@PathParam("id") queryId: Int, store: Store): String {
+        return try {
+            DAOStore.update(queryId, store)
+            "Registro atualizado com sucesso."
+        } catch (error: Exception) {
+            "Não foi possível atualizar o item. Tente novamente.\n${error.message}"
+        }
     }
 }
